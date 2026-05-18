dc54ac7982dd8465cdebce8cd7137dfc606df666
sudo-uwjames <jahmes@uw.edu>
2026-05-17 21:38:09 -0700

    update

--- D3 HW2/README.md
+++ D3 HW2/README.md
@@ -1,2 +1,5 @@
 # Creating an Visualization using D3 & SVG
 
+![Visualization](preview.png)
+
+<p align="center">(Line Chart Representing Monthly Spotify Streams: Minimum (Blue), Average (Gray), Maximum (Green))</p>
--- D3 HW2/main.js
+++ D3 HW2/main.js
@@ -1,80 +1,139 @@
 //Consts/global variables  
-            const w = 500;
-            const h = 500;
-            const margin = 50;
-            const parseTime = d3.timeParse("%Y-%m");//time parser
-            
-            d3.csv("Spotify-2023.csv").then(data => {
-            console.log("data", data)
-                
-                data.forEach(d => { 
-                d.date = parseTime(d.date);//parse this time data
-                d.max = +d.max;
-                d.min = +d.min;
-                d.avg = +d.avg;    
-                });
-            
-            //scales - with Scale Time
-            const xScale = d3.scaleTime()
-                             .domain(d3.extent(data, d => d.date)) 
-                             .range([margin, w-margin]); 
+const w = 1600;
+const h = 1200;
+const margin = 120;
+const parseTime = d3.timeParse("%Y-%m");
 
-            const yScale = d3.scaleLinear()
-                             .domain([0, d3.max(data, d => d.track)]) 
-                             .range([h-margin, margin]); 
+d3.csv("Spotify-2023.csv").then(rawData => {
 
 
-
-            //bottom axis with tick time formatting
-            const bottomAxis = d3.axisBottom()
-                                .scale(xScale)
-                                .tickFormat(d3.timeFormat("%b"));
-                                //.tickFormat(d3.timeFormat("%b-%Y"))
-                                //.ticks(d3.timeMonth.every(2));
-                                //.ticks(4);
-             
-
-            //left axis
-            const leftAxis = d3.axisLeft()
-                             .scale(yScale);
-                             //.ticks(4);
-            
-            
-            //SVG
-            const svg = d3.select("body")
-                    .append("svg")
-                    .attr("width", w)
-                    .attr("height", h);
+    const rolled = d3.rollups(rawData,
+        v => ({
+            max: d3.max(v, d => +d.streams),
+            mean: d3.mean(v, d => +d.streams),
+            min: d3.min(v, d => +d.streams)
+        }),
+        d => d.date
+    );
 
 
-            //line generator with optional curves
-            const line = d3.line()
-                            //.curve(d3.curveStepAfter)
-                            //.curve(d3.curveBasis)
-                            //.curve(d3.curveNatural)
-                            .x(d=>xScale(d.date))
-                            .y(d=>yScale(d.track));
-           
-            //line
-            svg.append("path")
-                .data([data])
-                .attr("d", line)
-                //.attr("d", line(data))
-                .attr("class", "line")
-                //.style("stroke", "green");//note this example of inline styling vs CSS
-                console.log("path",line(data))
-            
-            //call axes
-            
-            svg.append("g")
-                .attr("class", "axis") 
-                .attr("transform", "translate(0," + (h-margin) + ")") 
-                .call(bottomAxis);    
+    const data = rolled.map(([dateStr, stats]) => ({
+        date: dateStr,
+        max: stats.max,
+        mean: stats.mean,
+        min: stats.min
+    }));
 
-            
-            svg.append("g")
-               .attr("class", "axis") 
-               .attr("transform", "translate(" + margin + ",0)")
-               .call(leftAxis);
 
-            });
+    data.sort((a, b) => d3.ascending(a.date, b.date));
+
+    console.log("data", data)
+
+    data.forEach(d => {
+        d.date = parseTime(d.date);
+        d.max = +d.max;
+        d.mean = +d.mean;
+        d.min = +d.min;
+    });
+
+    //scales - with Scale Time
+    const xScale = d3.scaleTime()
+        .domain(d3.extent(data, d => d.date))
+        .range([margin, w - margin]);
+
+
+    const yScale = d3.scaleLinear()
+        .domain([0, d3.max(data, d => d.max)])
+        .range([h - margin, margin]);
+
+
+    //bottom axis with tick time formatting
+    const bottomAxis = d3.axisBottom()
+        .scale(xScale)
+        .tickFormat(d3.timeFormat("%b"));
+
+
+    //left axis
+    const leftAxis = d3.axisLeft()
+        .scale(yScale);
+    //.ticks(4);
+
+
+    //SVG
+    const svg = d3.select("body")
+        .append("svg")
+        .attr("width", w)
+        .attr("height", h);
+
+
+    const lineMax = d3.line()
+        .x(d => xScale(d.date))
+        .y(d => yScale(d.max));
+
+    const lineMean = d3.line()
+        .x(d => xScale(d.date))
+        .y(d => yScale(d.mean));
+
+    const lineMin = d3.line()
+        .x(d => xScale(d.date))
+        .y(d => yScale(d.min));
+
+    svg.append("path")
+        .data([data])
+        .attr("d", lineMax)
+        .attr("class", "line max");
+
+    svg.append("path")
+        .data([data])
+        .attr("d", lineMean)
+        .attr("class", "line mean");
+
+    svg.append("path")
+        .data([data])
+        .attr("d", lineMin)
+        .attr("class", "line min");
+
+    //call axes
+    svg.append("g")
+        .attr("class", "axis")
+        .attr("transform", "translate(0," + (h - margin) + ")")
+        .call(bottomAxis);
+
+    svg.append("g")
+        .attr("class", "axis")
+        .attr("transform", "translate(" + margin + ",0)")
+        .call(leftAxis);
+
+    svg.append("text")
+        .attr("class", "chart-title")
+        .attr("x", w / 2)
+        .attr("y", margin / 2)
+        .text("Spotify 2023 Monthly Stream Metrics");
+
+    svg.append("text")
+        .attr("class", "axis-label")
+        .attr("x", w / 2)
+        .attr("y", h - 60)
+        .text("Months (2023)");
+
+    svg.append("text")
+        .attr("class", "axis-label")
+        .attr("transform", "rotate(-90)")
+        .attr("x", -h / 2)
+        .attr("y", 20)
+        .text("Total Streams");
+
+    //call axes
+    svg.append("g")
+        .attr("class", "axis")
+        .attr("transform", "translate(0," + (h - margin) + ")")
+        .call(bottomAxis);
+
+    svg.append("g")
+        .attr("class", "axis")
+        .attr("transform", "translate(" + margin + ",0)")
+        .call(leftAxis);
+
+});
+
+
